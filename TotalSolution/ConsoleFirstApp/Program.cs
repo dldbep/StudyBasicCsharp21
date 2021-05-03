7c56f04b749d9ce0cf26db072582734dbce3cebf
dldbep <dldbep@naver.com>
2021-05-03 10:41:08 +0900

    수정

--- TotalSolution/ConsoleFirstApp/Program.cs
+++ TotalSolution/ConsoleFirstApp/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleFirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!"); //주석
+            Console.WriteLine("Second print");
+
         }
     }
 }
