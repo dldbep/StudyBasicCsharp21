c830b9acc30025995252c361ce0ecf2fcce2e3c4
dldbep <dldbep@naver.com>
2021-05-03 11:12:44 +0900

    주석 추가

--- TotalSolution/ConsoleFirstApp/Program.cs
+++ TotalSolution/ConsoleFirstApp/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleFirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!"); //주석
-            Console.WriteLine("Second print");
+            Console.WriteLine("Second print"); //두번째 주석
 
         }
     }
